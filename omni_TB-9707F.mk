ae804d0fab31bebef935fffd16aad59da37213a1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-05 14:45:43 +0200

    TB-9707F: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: lenovo Model: Lenovo TB-9707F Script version: 2.4.0

---
+++ omni_TB-9707F.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from TB-9707F device
+$(call inherit-product, device/lenovo/TB-9707F/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := TB-9707F
+PRODUCT_NAME := omni_TB-9707F
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := Lenovo TB-9707F
+PRODUCT_MANUFACTURER := lenovo
